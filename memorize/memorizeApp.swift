bf41e436eb13c3fa96af0e2dc084ef8b7e86b8c1
qonehan <kyuwonhan04@gmail.com>
2024-05-01 16:00:11 +0900

    Initial Commit

---
+++ memorize/memorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  memorizeApp.swift
+//  memorize
+//
+//  Created by 한규원 on 5/1/24.
+//
+
+import SwiftUI
+
+@main
+struct memorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
